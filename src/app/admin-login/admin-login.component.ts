9f2941859b9dca84010d023844849d47f5582bd7
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-28 12:10:46 +0530

    adminLogin

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent implements OnInit {
+
+  constructor(private myrouter:Router) { }
+  emailId=""
+  password=""
+  readAdminLogin=()=>{
+    let data={
+      "emailId":this.emailId,
+      "password":this.password
+    }
+    if (this.emailId=="Admin"&&this.password=="1234") {
+      this.myrouter.navigate(["/adminDashboard"])
+    } else {
+      alert("Invalid emailId or Password")
+    }
+    this.emailId=""
+    this.password=""
+  }
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-
-
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:AdminLoginComponent
+  },
+  {
+    path:"adminDashboard",component:AdminDashboardComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
     
   ],
   providers: [],
