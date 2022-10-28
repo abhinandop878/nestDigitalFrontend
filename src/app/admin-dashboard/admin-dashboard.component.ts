c13ea385670ee79fbb1ce2948d3f0f853eba06e4
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-28 13:53:21 +0530

    add employee

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,8 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
+  empId=""
+  name=""
+  place=""
+  bloodGroup=""
+  desigination=""
+  companyEmail=""
+  dob=""
+  companyAddress=""
+  gender=""
+  password=""
+  cpassword=""
+  readAddEmployee=()=>{
+    let data={
+      "empId":this.empId,
+      "name":this.name,
+      "place":this.place,
+      "bloodGroup":this.bloodGroup,
+      "desigination":this.desigination,
+      "companyEmail":this.companyEmail,
+      "dob":this.dob,
+      "companyAddress":this.companyAddress,
+      "gender":this.gender,
+      "password":this.password
+    }
+    this.myapi.addEmployee(data).subscribe((res)=>{
 
+    })
+    alert("Added Successfully !")
+    this.empId=""
+    this.name=""
+    this.place=""
+    this.bloodGroup=""
+    this.desigination=""
+    this.companyEmail=""
+    this.dob=""
+    this.companyAddress=""
+    this.gender=""
+    this.password=""
+    this.cpassword=""
+  }
   ngOnInit(): void {
   }
 
