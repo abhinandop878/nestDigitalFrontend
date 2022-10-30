c75d4ea166620612ffc458d12daffab8614e47bd
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-30 18:59:10 +0530

    loginSecurity

---
+++ src/app/login-security/login-security.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-login-security',
+  templateUrl: './login-security.component.html',
+  styleUrls: ['./login-security.component.css']
+})
+export class LoginSecurityComponent implements OnInit {
+
+  constructor(private myapi:ApiService,private myrouter:Router) { }
+  securityEmail=""
+  securityPassword=""
+  readSecurityLogin=()=>{
+    let data={
+      "securityEmail":this.securityEmail,
+      "securityPassword":this.securityPassword
+    }
+    this.myapi.securityLogin(data).subscribe((res:any)=>{
+      if (res.length>0) {
+        localStorage.setItem("securityID",res[0].securityId)
+        localStorage.setItem("securityName",res[0].securityName)
+        this.myrouter.navigate(["/"])
+      } else {
+        alert("Invalid EmailID or Password !")
+      }
+      this.securityEmail=""
+      this.securityPassword=""
+    })
+
+  }
+  ngOnInit(): void {
+  }
+
+}
